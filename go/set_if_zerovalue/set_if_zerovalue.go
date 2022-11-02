52d068c621b9ee018c941f20233a9a62db799f30
huang qing yun <zelin@huangs-MacBook-Air.local>
2022-11-02 21:29:00 +0800

    20221102

--- go/set_if_zerovalue/set_if_zerovalue.go
+++ go/set_if_zerovalue/set_if_zerovalue.go
@@ -1,14 +1,16 @@
 package set_if_zerovalue
 
+import "reflect"
+
 func SetStringIfZerovalue(old string, new string) string {
-	if old == "" {
+	if reflect.ValueOf(old).IsZero() {
 		return new
 	}
 	return old
 }
 
 func SetIntIfZerovalue(old int, new int) int {
-	if old == 0 {
+	if reflect.ValueOf(old).IsZero() {
 		return new
 	}
 	return old
